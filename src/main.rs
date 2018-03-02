3c7a67b3d0bac82ec01787ae526d6b727a1503c2
Mominul <mominul2082@gmail.com>
2018-03-02 21:22:00 +0600

    use structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[macro_use] extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "jabir", about = "Chemical elements analyser")]
+enum Jabir {
+    #[structopt(name = "show", about = "Show element")]
+    Show {
+        element: String,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Jabir::from_args();
+
+    println!("{:?}", matches);
+    
+    match matches {
+        Jabir::Show { element }  => {
+            println!("Element is {}", element);
+        }
+    }
 }
